1bd5df32126a07117efe8747ff03e59883eff52b
Willem Stuursma <willem@mollie.nl>
2012-12-12 10:57:35 +0100

    Bereid release v3.11 voor

--- app/code/community/Mollie/Mpm/Model/Idl.php
+++ app/code/community/Mollie/Mpm/Model/Idl.php
@@ -28,7 +28,7 @@
  * @category    Mollie
  * @package     Mollie_Mpm
  * @author      Mollie B.V. (info@mollie.nl)
- * @version     v3.10.0
+ * @version     v3.11.0
  * @copyright   Copyright (c) 2012 Mollie B.V. (http://www.mollie.nl)
  * @license     http://www.opensource.org/licenses/bsd-license.php  Berkeley Software Distribution License (BSD-License 2)
  *
