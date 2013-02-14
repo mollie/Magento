4eeac44271f1f280028e63073b63def245cfa45e
Willem Stuursma <willem@mollie.nl>
2013-02-14 16:24:35 +0100

    Verplaats installatiebestanden naar juiste plaats

---
+++ tests/unittests/installationTest.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * @covers Mollie_Mpm_Model_Idl
+	 */
+class InstallationTest extends MagentoPlugin_TestCase
+{
+	public function getVersion()
+	{
+		$xml = simplexml_load_file(PROJECT_ROOT . "/app/code/community/Mollie/Mpm/etc/config.xml");
+		return $xml->modules->Mollie_Mpm->version->__toString();
+	}
+
+	public function testInstallationFilesNamedCorrectly()
+	{
+		$version = $this->getVersion();
+
+		$this->assertFileExists(PROJECT_ROOT . "/app/code/community/Mollie/Mpm/sql/mpm_setup/mysql4-install-{$version}.php");
+		$this->assertFileExists(PROJECT_ROOT . "/app/code/community/Mollie/Mpm/sql/mpm_setup/mysql4-uninstall-{$version}.php");
+	}
+}
