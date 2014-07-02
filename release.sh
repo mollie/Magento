3b681396b7e3c27fcaf1d88bcd0a5e719d9ee68a
Lennard van Gunst <lennard@mollie.nl>
2014-07-02 14:49:59 +0200

    Change release file name

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip -9 -r release.zip app lib LICENSE readme.mdown upgrading.mdown changelog.mdown -x *.git* *.DS_Store
+zip -9 -r mollie-magento-x.x.x.zip app lib LICENSE readme.mdown upgrading.mdown changelog.mdown -x *.git* *.DS_Store
