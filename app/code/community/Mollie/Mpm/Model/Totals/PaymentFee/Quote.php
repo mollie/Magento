7aa0c80a4acc0284c295a06be89b8082993f7b9b
Marvin-Magmodules <marvin@magmodules.eu>
2019-12-02 19:31:53 +0100

    Fixed display of zero payment fee in checkout on certain setups

--- app/code/community/Mollie/Mpm/Model/Totals/PaymentFee/Quote.php
+++ app/code/community/Mollie/Mpm/Model/Totals/PaymentFee/Quote.php
@@ -45,7 +45,7 @@ class Mollie_Mpm_Model_Totals_PaymentFee_Quote extends Mage_Sales_Model_Quote_Ad
     public function fetch(Mage_Sales_Model_Quote_Address $address)
     {
         $amount = $address->getMollieMpmPaymentFee();
-        if ($amount) {
+        if (floatval($amount)) {
             $address->addTotal(
                 array(
                     'code' => $this->getCode(),
